e084426fc49ca99734ff2acf69a782bba2e9e067
Kangonga <guchu413@gmail.com>
2022-07-21 10:01:12 +0300

    complete exercise one and two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(str,num=Math.ceil(Math.random()*6)) {
+    let temp = ""
 
+    if (num < 0) {
+        temp = "ERROR";
+    }
+
+    else {
+        if (str == ''){
+            temp = '';
+        }
+        else {
+            for (i=0;i<num;i++){
+            temp += str;
+            }
+        }
+    }
+        return (temp)
 };
+console.log(repeatString('hey',2))
 
 // Do not edit below this line
 module.exports = repeatString;
