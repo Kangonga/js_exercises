b9d280b08db58626015b78fc0eab6e0db3cae458
Kangonga <guchu413@gmail.com>
2022-07-21 10:15:55 +0300

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    str = str.split('').reverse().join('');
+    return str;
 };
+console.log(reverseString('hey'))
 
 // Do not edit below this line
 module.exports = reverseString;
