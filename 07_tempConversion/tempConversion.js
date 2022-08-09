07e24e6f10c950789316c621574c2bde36cbe73d
Kangonga <guchu413@gmail.com>
2022-08-10 00:53:41 +0300

    complete temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let converts =(temp-32)*(5/9)
+  converts = Math.round(converts*10)/10
+  return converts
+  };
 
+const ctof = function(temp) {
+  let converts = (temp*(9/5))+32
+  converts = Math.round(converts*10)/10
+  return converts
 };
 
-const ctof = function() {
-
-};
-
+console.log(ctof(73.2))
 // Do not edit below this line
 module.exports = {
   ftoc,
