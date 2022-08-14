a2e92ce9f5073a778bdf0f609fb5c66448881665
Kangonga <guchu413@gmail.com>
2022-08-14 23:11:33 +0300

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    //generate fibonacci sequence
+    //a number num is the sum of the two numbers preceding it.
+    //three variables 011235
+    if(num<0){
+        return('OOPS')
+    }
+    let fib = []
+    let i=0
+    let start=0, next=1, third=0
+    while(i<num){
+        start=next
+        next=third
+        third = start+next
+        fib.push(third)
+        i++
+    }
+    return fib[fib.length-1]
 };
 
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
