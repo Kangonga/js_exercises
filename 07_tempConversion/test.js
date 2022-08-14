f369264146ffa2d0007690e0442937ff062f45ad
Kangonga <guchu413@gmail.com>
2022-08-14 17:19:38 +0300

    complete calculator

---
+++ 07_tempConversion/test.js
@@ -0,0 +1,7 @@
+let num = 14.235642
+
+numConverted = Math.round(num*10)/10
+
+console.log(numConverted)
+
+
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let a = arr.reduce((total,item)=>{
+    return(total+item)
+  },0)
+  return a
 };
 
-const multiply = function() {
-
+const multiply = function(c) {
+  let arr=[...c];
+  console.log(arr)
+  if (arr.length == 0){
+    arr[0] = 1
+  }
+  let arrFin = arr.reduce((total,item)=>{
+    return total*item
+  },1)
+  return arrFin
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(b) {
+  let fin = 1
+	for(let i=b;i>0;i--){
+    fin = fin * i
+  }
+  return fin
 };
 
 // Do not edit below this line
