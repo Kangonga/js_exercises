6bcb0b0895569a9ac0b951116a7a4df822db6095
Kangonga <guchu413@gmail.com>
2022-08-14 17:59:09 +0300

    completed palindormes in js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/\W|_/g, '').toLowerCase()
+    words = [...word]
+    let reversed = words.reverse().join('')
+    // console.log(reversed)
+    // console.log(word)
+    if(word == reversed){
+        return true
+    }
+    else {
+        return false
+    }
+    
 };
-
+console.log(palindromes('bar rab!'));
 // Do not edit below this line
 module.exports = palindromes;
